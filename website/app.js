91f2283f64cbb8f1a20be67ef3f631bf587e0e96
Rohan <rohanasif1990@gmail.com>
2022-10-25 21:25:51 +0500

    added pictures api

--- website/app.js
+++ website/app.js
@@ -2,6 +2,8 @@ const geoURL = "http://api.geonames.org/searchJSON?";
 const geoUsername = `rohanasif1990`;
 const weatherURL = "https://api.weatherbit.io/v2.0/forecast/daily?"
 const weatherKey = "20028a8267a24bba9a807362767bc4a7"
+const pixabayKey = "30776478-ff0b8818f9bba72161ebb1731"
+const pixabayURL = "https://pixabay.com/api?"
 
 let d = new Date();
 let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
@@ -24,6 +26,16 @@ submitBtn.addEventListener("click", (e) => {
             }).catch(function (error) {
                 console.log(error);
                 alert("Invalid city");
+            })
+        getPictures(city, pixabayURL, pixabayKey)
+            .then(function (picsData) {
+                return postPictureData("/addPicture", { pic: picsData['hits'][0]["webformatURL"] })
+            })
+            .then(function () {
+                return receivePictureData()
+            }).catch(function (error) {
+                console.log(error);
+                alert("No pictures found")
             })
     }
 })
@@ -80,5 +92,49 @@ const getWeather = async (weatherURL, weatherKey, lat, lon) => {
     }
     catch (error) {
         console.log("error", error);
+    }
+}
+
+const getPictures = async (city, pixabayURL, pixabayKey) => {
+    const query = city.split(" ").join("+");
+    const res = await fetch(`${pixabayURL}key=${pixabayKey}&q=${query}`);
+    try {
+        const picsData = await res.json();
+        return picsData;
+    }
+    catch (error) {
+        console.log("error", error)
+    }
+}
+
+const receivePictureData = async () => {
+    const request = await fetch("/allPictures");
+    try {
+        const allData = await request.json()
+        document.getElementById("picture").src = allData['temp'];
+    }
+    catch (error) {
+        console.log("error", error)
+    }
+}
+
+const postPictureData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            pic: data.pic
+        })
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    }
+    catch (error) {
+        console.log(error);
     }
 }
