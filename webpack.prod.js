8de229a4aca16271b6e950343cb6bd51a2aa08b8
Rohan <rohanasif1990@gmail.com>
2022-10-27 01:46:46 +0500

    prod buld produces single line js file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,63 +1,23 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const dotenv = require("dotenv")
-const { ServiceWorkerPlugin } = require("service-worker-webpack");
-dotenv.config();
 
 module.exports = {
-    entry: {
-        main: './website/app.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-        clean: true,
-        assetModuleFilename: "[name][ext]"
-    },
+    entry: './website/app.js',
     mode: 'production',
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            },
-            {
-                test: '/.scss$/',
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
             }
         ]
     },
     plugins: [
-        new ServiceWorkerPlugin(),
-        new HtmlWebPackPlugin(),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
-        new webpack.DefinePlugin({
-            'process.env': JSON.stringify(process.env)
+        new HtmlWebPackPlugin({
+            template: "./website/index.html",
+            filename: "./index.html",
         })
     ]
 }
