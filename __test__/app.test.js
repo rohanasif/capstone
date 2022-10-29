3e2ddcdb3e1327d6256f1e9750f6eb2f217af1c1
Rohan <rohanasif1990@gmail.com>
2022-10-29 21:59:24 +0500

    remove unnecessary imports in app.test.js

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,8 +1,8 @@
 import { TestEnvironment } from "jest-environment-jsdom";
-import { mainFunction, getCity, geoURL, geoUsername, postWeatherData, receiveWeatherData, getWeather, updatePictureText, postPictureData, receivePictureData, getPictures, forLoop} from "../website/client/js/app.js"
+import { geoURL, geoUsername } from "../website/client/js/app.js"
 require('jest-fetch-mock').enableMocks();
 
-test("Fetched data should be", async ()=> {
+test("Fetched data should be", async () => {
     expect.assertions(1);
     const data = await fetchMock(`${geoURL}q=new+york&username=${geoUsername}`);
     expect(data.status).toEqual(200);
