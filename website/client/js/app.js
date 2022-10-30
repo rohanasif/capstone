64fc89af8f59e0c9bc890858493175676ff1c7e7
Rohan <rohanasif1990@gmail.com>
2022-10-31 03:56:51 +0500

    added trip length function but not working

--- website/client/js/app.js
+++ website/client/js/app.js
@@ -15,6 +15,7 @@ const pixabayURL = "https://pixabay.com/api?";
 const submitBtn = document.getElementById("submitBtn");
 const resetBtn = document.getElementById("resetBtn");
 const time = document.getElementById("time");
+const tripLength = document.getElementById("trip-length");
 
 
 
@@ -51,14 +52,23 @@ const mainFunction = (e) => {
     const [depart_year, depart_month, depart_day] = depart_date.split("-")
     const [depart_hour, depart_minute] = depart_time.split(":")
 
-    const future = new Date(depart_year, depart_month - 1, depart_day, depart_hour, depart_minute);
-    if (city === "" || departure === "" || future < present) {
+    const arrival = document.getElementById("arrival").value;
+
+    const [arrival_date, arrival_time] = arrival.split("T")
+    const [arrival_year, arrival_month, arrival_day] = arrival_date.split("-")
+    const [arrival_hour, arrival_minute] = arrival_time.split(":")
+
+
+    const formattedDeparture = new Date(depart_year, depart_month - 1, depart_day, depart_hour, depart_minute);
+    const formattedArrival = new Date(arrival_year, arrival_month - 1, arrival_day, arrival_hour, arrival_minute);
+
+    if (city === "" || formattedDeparture < present || formattedArrival < present || formattedArrival < formattedDeparture) {
         alert("Invalid input");
     }
     else {
 
-        time.innerHTML = `<b>Departure in ${Math.ceil((future - present) / 3600 / 1000 / 24)} day(s)</b>`
-
+        time.innerHTML = `<b>Departure in ${Math.ceil((future - present) / 3600 / 1000 / 24)} day(s)</b>`;
+        tripLength.innerHTML = `<b>Trip length is ${Math.ceil((formattedArrival - formattedDeparture) / 3600 / 1000 / 24)} day(s)<b>`;
         forLoop();
         updatePictureText();
         getPictures(pixabayKey, pixabayURL)
